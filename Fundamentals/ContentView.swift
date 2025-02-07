f64c24f384e155bf7a0c9c015de4633cc1820826
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-06 23:55:11 -0300

    .border(.orange, width: 2)

--- Fundamentals/ContentView.swift
+++ Fundamentals/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "Yo"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text(messageString)
+                .font(.system(.largeTitle, weight: .heavy))
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 2)
+                .padding()
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .border(.purple, width: 2)
         }
-        .padding()
+//        .padding()
     }
 }
 
