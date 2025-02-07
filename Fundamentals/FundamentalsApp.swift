6fcc8b14a218a0556efc7d0194e6b42fa0531fec
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-06 22:50:16 -0300

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Christian Manzaraz on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
